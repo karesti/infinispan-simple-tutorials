bba235e1b4d291998e1e447ae54eb7a1870b7ca7
Pedro Ruivo <pruivo@redhat.com>
2019-11-25 14:00:51 +0000

    Fixes

--- xsite-replication/run.sh
+++ xsite-replication/run.sh
@@ -1,7 +1,1 @@
-docker volume rm xsite-vol-lon
-docker volume rm  xsite-vol-nyc
-docker volume create xsite-vol-lon
-docker volume create xsite-vol-nyc
-cp infinispan-xsite-LON.xml custom-udp-LON.xml /var/lib/docker/volumes/xsite-vol-lon/_data
-cp infinispan-xsite-NYC.xml custom-udp-NYC.xml /var/lib/docker/volumes/xsite-vol-nyc/_data
 docker-compose up
